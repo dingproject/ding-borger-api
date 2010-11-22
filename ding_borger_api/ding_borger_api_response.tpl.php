396989fc8fff69b66cbef2e42c0071792f568eea
Kasper Garnæs <kasper@reload.dk>
2010-11-22 12:45:46 +0100

    Use theme layer to render the response.

---
+++ ding_borger_api/ding_borger_api_response.tpl.php
@@ -0,0 +1,12 @@
+<div class="ding-borger-api-data"> 
+  <?php print theme_status_messages() ?>
+  <div class="ding-debts">
+    <?php print $debt_notice ?>
+  </div>
+  <div class="ding-loans">
+    <?php print $loans ?>
+  </div>
+  <div class="ding-reservations">
+    <?php print $reservations ?>
+  </div> 
+</div>
