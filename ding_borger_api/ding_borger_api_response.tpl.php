5cdb9c66c5ff1afeab7f3549ff3bf1e2d2b55833
Kasper Garnæs <kasper@reload.dk>
2010-11-23 16:35:13 +0100

    Implemented initial version of borger.dk stylesheet and import it when rendering a response.

--- ding_borger_api/ding_borger_api_response.tpl.php
+++ ding_borger_api/ding_borger_api_response.tpl.php
@@ -1,8 +1,13 @@
+<style type="text/css">
+  @import url(<?php print url(drupal_get_path('module', 'ding_borger_api').'/css/ding_borger_api.css') ?>);
+</style>
 <div class="ding-borger-api-data"> 
   <?php print theme_status_messages() ?>
-  <div class="ding-debts">
-    <?php print $debt_notice ?>
-  </div>
+	  <?php if ($debt_notice) : ?>
+	  <div class="ding-debts">
+	    <?php print $debt_notice ?>
+	  </div>
+  <?php endif; ?>
   <div class="ding-loans">
     <?php print $loans ?>
   </div>
