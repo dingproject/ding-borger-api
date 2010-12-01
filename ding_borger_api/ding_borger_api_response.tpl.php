1cd518a2c38387f24d50dbc2e6b74a69d42d264e
Kasper Garnæs <kasper@reload.dk>
2010-12-01 11:59:49 +0100

    Added support for converting CSS style sheet content to inline style attributes using Emogrifier library.
    Also refactored response postprocessing.

--- ding_borger_api/ding_borger_api_response.tpl.php
+++ ding_borger_api/ding_borger_api_response.tpl.php
@@ -1,6 +1,3 @@
-<style type="text/css">
-  @import url(<?php print url(drupal_get_path('module', 'ding_borger_api').'/css/ding_borger_api.css') ?>);
-</style>
 <div class="ding-borger-api-data"> 
   <?php print theme_status_messages() ?>
 	  <?php if ($debt_notice) : ?>
